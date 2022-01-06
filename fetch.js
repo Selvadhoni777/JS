3e76750d3c874532c987c5bce0e98c6447f7dcbf
selvadhoni777 <selvadhoni640@gmail.com>
2022-01-06 12:27:22 +0530

    Table method

--- fetch.js
+++ fetch.js
@@ -1,27 +1,30 @@
-// fetch("https://jsonplaceholder.typicode.com/todos")
-//   .then((Response) => Response.json())
-//   .then((json) => {
-//     console.log(json);
-
-//     var table = document.getElementById("table");
-
-//     for (i = 0; i < json.length; i++) {
-//       var row = table.insertRow(i);
-//       var cell0 = row.insertCell(0);
-//       var cell1 = row.insertCell(1);
-//       var cell2 = row.insertCell(2);
-//       cell0.innerHTML = json[i].id;
-//       cell1.innerHTML = json[i].completed;
-//       cell2.innerHTML = json[i].title;
-//     }
-//   });
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then((response) => response.json())
   .then((getData) => {
-    // console.log(getData);
-    let q = getData;
+    console.log(getData);
+
+    var tbl = document.createElement("table");
+    document.body.appendChild(tbl);
+    var row = document.createElement("tr");
+    tbl.appendChild(row);
+
+    // ---------------->GET TABLE HEADER
+
+    var getHead = Object.keys(Object.assign({}, ...getData));
+    for (let i = 0; i < getHead.length; i++) {
+      var head = document.createElement("th");
+      row.appendChild(head);
+      head.append(getHead[i]);
+    }
+    // ---------------------->GET TABLE DATA
+    for (a = 0; a < getData.length; a++) {
+      var dataRow = document.createElement("tr");
+      tbl.appendChild(dataRow);
+      var dat = Object.values(getData[a]);
+      dat.forEach((d) => {
+        var data = document.createElement("td");
+        dataRow.appendChild(data);
+        data.append(d);
+      });
+    }
   });
-
-var tbl = document.createElement("table");
-
-var row = document.createElement("tr");
